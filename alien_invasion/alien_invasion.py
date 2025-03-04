50df96baeffea4cb679b99fe6748c26c7aa4af23
BywStudio <19941829077@163.com>
2025-03-04 15:21:50 +0800

    创建 Settings 类并导入主文件使用

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from settings import Settings
 
 
 # 管理游戏资源和行为的类
@@ -11,14 +12,15 @@ class AlienInvasion:
         pygame.init()
         # 定义时钟
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
         # 显示窗口，宽：1200px，高: 800px
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,
+            self.settings.screen_height
+        ))
         # 创建窗口标题：Alien Invasion
         pygame.display.set_caption('Alien Invasion')
-
-        #设置背景色
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # 开始游戏的主循环
@@ -29,7 +31,7 @@ class AlienInvasion:
                     sys.exit()
 
             # 每次循环时都重新绘制屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
             self.clock.tick(60)
